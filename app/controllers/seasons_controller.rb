df9b919a36d9f44c44b25766ac354a35ec787b56
Joseph Kiefer <josephkiefer9@gmail.com>
2013-05-12 23:41:28 -0700

    Stuff for the uploading pictures

--- app/controllers/seasons_controller.rb
+++ app/controllers/seasons_controller.rb
@@ -1,5 +1,6 @@
 class SeasonsController < ApplicationController
 	def show
 		@queens = Queen.where(:season_id => params[:id])
+		@queen = Queen.find(params[:id])
 	end
 end
--- app/models/queen.rb
+++ app/models/queen.rb
@@ -1,4 +1,4 @@
 class Queen < ActiveRecord::Base
   attr_accessible :bio, :is_allstar, :is_winner, :name, :season_id, :picture
-  has_attached_file :picture, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png", :url => "/data/:style/:basename.:extension", :path => ":rails_root/data/:style/:basename.:extension"
+  has_attached_file :picture, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png", :url => "/data/:style/:basename.:extension", :path => ":rails_root/public/data/:style/:basename.:extension"
 end
